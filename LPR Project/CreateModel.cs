e0fc9490d706e4b497edb1d781ac9dca59408195
EnriqueDeSousa <603110@student.belgiumcampus.ac.za>
2026-08-10 12:51:00 +0200

    Created main menu and create model menu

--- LPR Project/CreateModel.cs
+++ LPR Project/CreateModel.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LPR_Project
 {
-    public partial class Form1 : Form
+    public partial class CreateModel : Form
     {
-        public Form1()
+        public CreateModel()
         {
             InitializeComponent();
         }
